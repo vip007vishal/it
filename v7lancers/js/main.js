00846e5e4fd5911d26e743c42d23940eab9c5e49
unknown <vishal15v2006@gmail.com>
2025-07-03 01:05:22 +0530

    c

--- v7lancers/js/main.js
+++ v7lancers/js/main.js
@@ -3771,3 +3771,13 @@ function initMap() {
         }
     });
 }
+
+
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
